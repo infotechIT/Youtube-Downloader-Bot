0b55fdf48d954dcec057dd68238c20b71907a26f
InfoTech <infotechtele@gmail.com>
2022-05-11 16:04:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,12 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/infotechIT")],
+        [InlineKeyboardButton("Channel", url="https://t.me/infotechIT"), InlineKeyboardButton("Creator", url="https://telegram.me/alxneboy") ],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/alxneboy")]
+            "Source Code", url="https://github.com/infotechIT/YoutubeDownloaderBot")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"""Hey <b>{message.from_user.first_name}</b>\nA Simple YouTube Downloader Bot that can:
+  ➠ Download YouTube videos
+  ➠ Download audio from YouTube videos \n\n Made with ♥️ by @infotechIT"""
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
